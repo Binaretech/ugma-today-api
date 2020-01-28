87e3553555b38f218591880cda4280924a99414c
Angel Afonso <angelafonso60@gmail.com>
2020-01-28 07:11:30 -0400

    WIP: added user model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models.user import User
 
-# Register your models here.
+admin.site.register(User, UserAdmin)
--- ugma_today/settings.py
+++ ugma_today/settings.py
@@ -89,6 +89,8 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'api.User'
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
