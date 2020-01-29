a2d03318828d8ea74caa6d86c1f279c20bf6e06f
Angel Afonso <angelafonso60@gmail.com>
2020-01-28 20:47:25 -0400

    changed license and reverted custom user model

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,2 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models.user import User
-
-admin.site.register(User, UserAdmin)
--- api/models/user.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractBaseUser
-from django.db import models
-
-class User(AbstractBaseUser):
-    username = models.CharField(max_length=100, unique=True)
-    status = models.BooleanField()
-    type = models.PositiveSmallIntegerField()
-    
-    USERNAME_FIELD = 'username'    
