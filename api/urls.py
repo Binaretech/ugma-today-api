9febfc6472102a8e2093d533cbafd851f295e9f5
Angel Afonso <angelafonso60@gmail.com>
2020-01-30 06:54:13 -0400

    WIP: AuthViewSet

--- api/urls.py
+++ api/urls.py
@@ -1,2 +1,7 @@
+from rest_framework.authtoken import views
+from rest_framework.urls import url
+from .views.auth import AuthViewSet
+
 urlpatterns = [
+    url('login/', AuthViewSet.as_view({'post': 'login'})),
 ]
