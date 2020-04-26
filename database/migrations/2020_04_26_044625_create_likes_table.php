067fb11ccdcd9eb9f84b04c41f8d9b86e3014bf5
Hector Zurga <hjzurga@gmail.com>
2020-04-26 00:54:56 -0400

    added likes migration

--- database/migrations/2020_04_26_044625_create_likes_table.php
+++ database/migrations/2020_04_26_044625_create_likes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProfilesTable extends Migration
+class CreateLikesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,14 @@ class CreateProfilesTable extends Migration
      */
     public function up()
     {
-        Schema::create('profile', function (Blueprint $table) {
+        Schema::create('likes', function (Blueprint $table) {
             $table->unsignedBigInteger('user_id');
-            $table->string('name', 64);
-            $table->string('lastname', 64);
-            $table->string('email', 45)->unique();
+            $table->morphs('likeable');
             $table->timestamps();
 
             $table->foreign('user_id')
                 ->references('id')
-                ->on('users')
-                ->onDelete('cascade');
+                ->on('users');
         });
     }
 
@@ -34,6 +31,6 @@ class CreateProfilesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('profile');
+        Schema::dropIfExists('likes');
     }
 }
