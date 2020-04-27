0581de9fb90f28472ff8e9a893be1e70a2519423
Angel <angelafonso60@gmail.com>
2020-04-26 20:55:11 -0400

    Added user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,10 +19,27 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'username' => $faker->userName,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'type' => $faker->numberBetween(0, 1),
+        'status' => $faker->numberBetween(0, 1),
+        'password' => 'secret',
         'remember_token' => Str::random(10),
     ];
 });
+
+$factory->state(User::class, 'admin', [
+    'type' => User::TYPES['ADMIN'],
+]);
+
+$factory->state(User::class, 'user', [
+    'type' => User::TYPES['USER'],
+]);
+
+$factory->state(User::class, 'active', [
+    'status' => User::STATUS['ACTIVE'],
+]);
+
+$factory->state(User::class, 'banned', [
+    'status' => User::STATUS['BANNED'],
+]);
