2b34de7d50ae4a142fd4a6ac374bc64ae0cf206e
Angel <angelafonso60@gmail.com>
2020-04-26 20:11:54 -0400

    added feedback model and migration

---
+++ database/migrations/2020_04_27_000521_create_feedbacks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeedbacksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedbacks', function (Blueprint $table) {
+            $table->id();
+            $table->text('comment');
+            $table->unsignedBigInteger('user_id')->nullable();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set_null');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedbacks');
+    }
+}
