6f3a1dfaff2066b2070dd7ae1c6315a6d2676d56
Hector Zurga <hjzurga@gmail.com>
2020-07-26 17:33:19 -0400

    Added advice seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,11 +12,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $time = Carbon::now();
+
         $this->call([
             UsersTableSeeder::class,
             PostTableSeeder::class,
             LikeTableSeeder::class,
-            CostTableSeeder::class
+            CostTableSeeder::class,
+            AdviceSeeder::class
         ]);
+        $time = $time->diffForHumans(Carbon::now());
+
+        echo "\nTime since seeders started: $time\n\n";
     }
 }
