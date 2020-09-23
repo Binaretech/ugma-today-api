564ede9d8abf4985b0ef75f37d7e4d6904c819a3
Hector Zurga <hjzurga@gmail.com>
2020-09-23 11:20:39 -0400

    fix: adapted all models to extends Custom Class Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,7 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Illuminate\Foundation\Auth\Access\Authorizable;
+use App\Models\Model;
 
 class User extends Model implements
     AuthenticatableContract,
