71e33271135f56bb28082e02977dc43ed33dae5f
Ángel Afonso <angelafonso60@gmail.com>
2020-12-20 15:29:53 -0400

    Comment replies handling (#24)
    * feat: Added posts and news listing with filters
    
    * feat: improved Posts responses
    
    * refactor: minor adjustments
    
    * improved posts and news methods
    
    * feat: updated post controllers
    
    * feat: like and unlike comment
    
    * Updated comment controllers
    
    * updated CommentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,23 +5,89 @@ namespace App\Http\Controllers;
 use App\Exceptions\Exception;
 use App\Http\Resources\CommentResource;
 use App\Models\Comment;
+use App\Models\Like;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-	public function store(Request $request, Post $post) {
+	public function index(Request $request, Post $post)
+	{
+		return CommentResource::collection(
+			$post->comments()->orderBy('created_at', 'ASC')->paginate($request->pagination ?? 10)
+		)->resource;
+	}
+
+	public function index_replies(Request $request, Comment $comment)
+	{
+		return CommentResource::collection(
+			$comment->replies()->orderBy('created_at', 'ASC')->paginate($request->pagination ?? 10)
+		)->resource;
+	}
+
+	public function store(Request $request, Post $post)
+	{
 		$request_data = $request->validate(Comment::STORE_RULES);
 		$comment = $post->comments()->save(new Comment(array_merge(
-			$request_data, [
+			$request_data,
+			[
 				'user_id' => $request->user()->id
 			]
 		)));
 
-		if(!$comment) {
+		if (!$comment) {
 			throw new Exception(trans('exception.CommentController.store'));
 		}
 
 		return new CommentResource($comment);
-	}	
+	}
+
+	public function reply(Request $request, Comment $comment)
+	{
+		$request_data = $request->validate(Comment::STORE_RULES);
+
+		$reply = $comment->replies()->save(new Comment(array_merge(
+			$request_data,
+			[
+				'user_id' => $request->user()->id,
+				'post_id' => $comment->post_id,
+			]
+		)));
+
+		if (!$reply) {
+			throw new Exception(trans('exception.CommentController.store'));
+		}
+
+		return new CommentResource($reply);
+	}
+
+	public function like(Request $request, Comment $comment)
+	{
+		$user = $request->user();
+
+		if ($comment->likes()->where('user_id', $user->id)->exists()) {
+			throw new Exception(trans('exception.CommentController.already_liked'), 400);
+		}
+
+		$comment->likes()->save(new Like(['user_id' => $user->id]));
+
+		return response()->json(['message' => trans('responses.success')], 201);
+	}
+
+	public function unlike(Request $request, Comment $comment)
+	{
+		$user = $request->user();
+
+		$like =	$comment->likes()->where(['user_id' => $user->id])->first();
+
+		if (!$like) {
+			throw new Exception(trans('exception.CommentController.already_unliked'), 400);
+		}
+
+		if (!$like->delete()) {
+			throw new Exception(trans('exception.internal_error'));
+		}
+
+		return response()->json(['message' => trans('responses.success')], 200);
+	}
 }
--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -6,25 +6,28 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class CommentResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function toArray($request)
-    {
+	/**
+	 * Transform the resource into an array.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return array
+	 */
+	public function toArray($request)
+	{
 		return [
 			'id' => $this->id,
 			'user' => new UserResource($this->user->load('profile')),
 			'comment' => $this->comment,
-			'replies' => $this->replies->count(),
+			'replies' => CommentResource::collection($this->replies()->paginate(2))->resource,
+			'repliesCount' => $this->replies->count(),
 			'likes' => $this->likes()->count(),
-			'replies' => $this->replies()->count(),
 			$this->mergeWhen($request->get('withTimestamps') === 'true', [
-                'createdAt' => $this->created_at,
-                'updatedAt' => $this->updated_at,
-            ]),	
+				'createdAt' => $this->created_at,
+				'updatedAt' => $this->updated_at,
+			]),
+			$this->mergeWhen(isset($this->reply_to_id), [
+				'reply_to_id' => $this->reply_to_id,
+			])
 		];
-    }
+	}
 }
